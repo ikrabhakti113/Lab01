0a4cd8b5e734aa8aaa629c47f9f9073b1998a0b2
Ikra Bhaktiananda <ikra.bhaktiananda@ui.ac.id>
2022-09-15 13:36:35 +0700

    lab02

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,4 +1,7 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from django.core import serializers
+from django.http import HttpResponse
 from wishlist.models import BarangWishlist
 
 def show_wishlist(request):
@@ -8,3 +11,20 @@ def show_wishlist(request):
         'nama': 'Ikra Bhaktiananda'
     }
     return render(request, "wishlist.html", context)
+
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    
